fb5173889f0c8993b0a75ee8369f3eea217202b6
aereal <aereal@aereal.org>
2020-10-21 20:33:59 +0900

    feat: add AttributeValueFrom

---
+++ conversions.go
@@ -0,0 +1,52 @@
+package ddbconversions
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+func AttributeValueFrom(from events.DynamoDBAttributeValue) *dynamodb.AttributeValue {
+	av := &dynamodb.AttributeValue{}
+	switch from.DataType() {
+	case events.DataTypeBinary:
+		av.SetB(from.Binary())
+	case events.DataTypeBinarySet:
+		av.SetBS(from.BinarySet())
+	case events.DataTypeBoolean:
+		av.SetBOOL(from.Boolean())
+	case events.DataTypeList:
+		vs := []*dynamodb.AttributeValue{}
+		for _, v := range from.List() {
+			lv := AttributeValueFrom(v)
+			vs = append(vs, lv)
+		}
+		av.SetL(vs)
+	case events.DataTypeMap:
+		mv := map[string]*dynamodb.AttributeValue{}
+		for k, v := range from.Map() {
+			mv[k] = AttributeValueFrom(v)
+		}
+		av.SetM(mv)
+	case events.DataTypeNull:
+		av.SetNULL(from.IsNull())
+	case events.DataTypeNumber:
+		av.SetN(from.Number())
+	case events.DataTypeNumberSet:
+		var ns []*string
+		for _, v := range from.NumberSet() {
+			n := v
+			ns = append(ns, &n)
+		}
+		av.SetNS(ns)
+	case events.DataTypeString:
+		av.SetS(from.String())
+	case events.DataTypeStringSet:
+		var ss []*string
+		for _, v := range from.StringSet() {
+			s := v
+			ss = append(ss, &s)
+		}
+		av.SetSS(ss)
+	}
+	return av
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/aereal/go-aws-dynamodb-conversions
+
+go 1.15
+
+require (
+	github.com/aws/aws-lambda-go v1.19.1
+	github.com/aws/aws-sdk-go v1.35.11
+)
